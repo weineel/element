3a81bab4d56ed1fb2fd14bea00b80f95cd0be197
weineel <LiJF_wn@163.com>
2020-10-19 10:57:14 +0800

    重现 popup 的bug

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,6 +1,28 @@
 <template>
-  <div style="margin: 20px;">
-    <el-input v-model="input" placeholder="请输入内容"></el-input>
+  <div class="play">
+    <el-button @click="drawerVisible = true">
+      Show Main Drawer
+    </el-button>
+    <el-drawer :visible.sync="drawerVisible" title="Main Drawer">
+      <el-radio-group v-model="showNumber">
+        <el-radio label="1"> Show 1 </el-radio>
+        <el-radio label="2"> Show 2 </el-radio>
+        <el-radio label="3"> Show 3 </el-radio>
+      </el-radio-group>
+
+      <div v-if="showNumber === '1'">
+        <el-button @click="dialogVisible = true">
+          Show Main Dialog 1
+        </el-button>
+        <el-dialog :visible.sync="dialogVisible" append-to-body> 1 </el-dialog>
+      </div>
+      <div v-if="showNumber === '2'">
+        2
+      </div>
+      <div v-if="showNumber === '3'">
+        3
+      </div>
+    </el-drawer>
   </div>
 </template>
 
@@ -8,8 +30,17 @@
   export default {
     data() {
       return {
-        input: 'Hello Element UI!'
+        drawerVisible: false,
+        dialogVisible: false,
+        showNumber: '1',
       };
     }
   };
 </script>
+
+<style>
+  .play {
+    margin: 20px;
+    height: 2000px
+  }
+</style>
